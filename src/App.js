3be0358b0a11e8c2448198029511b9a888de208c
tienplt <tienplt@scb.com.vn>
2022-08-30 17:39:44 +0700

    Build header

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App h-screen">
+      <header className="pl-6 pr-6 fixed top-0 left-0 right-0 z-100 bg-gradient-to-b from-header to-header-linear h-[100px]">
+        <div>
+          sadas
+        </div>
+
+      </header>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx, ts, tsx }"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "header": "#f53d2d",
+        "header-linear": "#f63"
+      }
+    },
   },
   plugins: [],
 }
