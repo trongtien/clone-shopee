8dab053c393e56388cd2534a34b08e4c8ff1fde6
pltTien <phanletrongtien@gmail.com>
2022-08-31 16:53:49 +0700

    Create Component Header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="main-root h-screen">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/HeaderNavLink.jsx
@@ -0,0 +1,22 @@
+
+
+
+export default function HeaderNavLink(){
+    return (
+        <nav className="container w-screen h-3 flex justify-between">
+            <div className="flex items-center flex-start p-1">
+                <p className="text-white font-light text-sm font-serif p-1">Kênh người bán</p>
+                <p className="text-white font-light text-sm font-serif p-1">Trờ thành shopee clone</p>
+                <p className="text-white font-light text-sm font-serif p-1">Tải ứng dụng</p>
+                <p className="text-white font-light text-sm font-serif p-1">Kết nối</p>
+            </div>
+
+            <div className="flex items-center flex-end p-1">
+                <p className="text-white font-light text-sm font-serif p-1">Thông báo</p>
+                <p className="text-white font-light text-sm font-serif p-1">Hỗ trợ</p>
+                <p className="text-white font-light text-sm font-serif p-1">Đắng ký</p>
+                <p className="text-white font-light text-sm font-serif p-1">Đăng nhập</p>
+            </div>
+        </nav>
+    )
+}
